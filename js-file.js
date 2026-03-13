73cf6e26ad4dcbbacd1065de90d1cc5d5c4781f0
akramshaheen <akram.shaheen27@gmail.com>
2026-03-13 08:51:55 +0100

    chore: arrange the squares on a 16x16 grid

--- js-file.js
+++ js-file.js
@@ -1,7 +1,11 @@
 const container = document.querySelector(".container");
+const width = 640;
+container.style.cssText = `width: ${width}px;`;
 
 const square = document.createElement("div");
 square.classList.add("square");
+const squareWidth = width / 16;
+square.style.cssText = `width: ${squareWidth}px;`;
 
 const fragment = document.createDocumentFragment();
 

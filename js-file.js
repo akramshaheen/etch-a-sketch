a7344011179d72a14991493eedae90749e734a2c
akramshaheen <akram.shaheen27@gmail.com>
2026-03-13 08:17:09 +0100

    chore: implement clone to improve performance

--- js-file.js
+++ js-file.js
@@ -1,7 +1,13 @@
 const container = document.querySelector(".container");
 
+const square = document.createElement("div");
+square.classList.add("square");
+
+const fragment = document.createDocumentFragment();
+
 for (let i = 1; i <= 256; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  container.append(square);
+  const clone = square.cloneNode(true);
+  fragment.append(clone);
 }
+
+container.append(fragment);

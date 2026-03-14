d4e87835fe3a8691c1aafef5a727b37ad4133e10
akramshaheen <akram.shaheen27@gmail.com>
2026-03-14 04:17:54 +0100

    feat: add 'Set Grid' button to change dimensions

--- js-file.js
+++ js-file.js
@@ -1,22 +1,31 @@
+const setGrid = document.querySelector("#set-grid");
+
 const container = document.querySelector(".container");
-const containerWidth = 640;
-container.style.cssText = `width: ${containerWidth}px;`;
+let grid = 16;
 
 const square = document.createElement("div");
 square.classList.add("square");
-const squareWidth = containerWidth / 16;
-square.style.cssText = `width: ${squareWidth}px;`;
+
+const generateGrid = () => {
+  square.style.flexBasis = `calc(100% / ${grid})`;
+  const fragment = document.createDocumentFragment();
+  for (let i = 1; i <= grid * grid; i++) {
+    const clone = square.cloneNode(true);
+    fragment.append(clone);
+  }
+  container.append(fragment);
+};
+
+generateGrid();
+
+setGrid.addEventListener("click", () => {
+  grid = +prompt("Choose a number between 16 and 100:");
+  container.innerHTML = "";
+  generateGrid();
+});
 
 container.addEventListener("mouseover", (e) => {
   if (e.target.matches(".square")) {
     e.target.classList.add("hover");
   }
 });
-
-const fragment = document.createDocumentFragment();
-
-for (let i = 1; i <= 256; i++) {
-  const clone = square.cloneNode(true);
-  fragment.append(clone);
-}
-container.append(fragment);
